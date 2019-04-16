51699c24f7242c6f7b5866bebd7605ef73779ed0
Kadson Breno Benvindo de Lima <kr4m3r@A307-28L.imd.local>
2019-04-16 19:40:54 -0300

    add

---
+++ queue.h
@@ -0,0 +1,15 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+struct queue{
+	queue(int capacity();
+	vector data;
+	void clear();
+	bool empty();
+	void push(int elemento);
+	int pop();
+	int top();
+	int size();
+};
+
+#endif
